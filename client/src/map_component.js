b5e1d6af0176a071e5342490f90e5f7678fc8031
Bahaà Addin Balashoni <bahaa.addin.balashoni@gmail.com>
2018-08-07 11:52:15 +0200

    Migrated to Mapbox GL

--- client/src/map_component.js
+++ client/src/map_component.js
@@ -16,93 +16,72 @@ class MapComponent extends React.Component {
 
 	set map({containerId, coords}) {
 		const {lat, lng} = coords;
-		this._map = new L.Map(containerId);
+		mapboxgl.accessToken = 'pk.eyJ1IjoiYmFoYWEtYWRkaW4iLCJhIjoiY2pqOWd6NmtmMnhnbzNsdW9sdHMya2FvbCJ9.JqtQYjBcIAeYIIEeauccXw';
 
-		const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-		const attribution = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-
-		const osm = new L.TileLayer(osmUrl, {
-			minZoom: 8,
-			maxZoom: 12,
-			attribution,
+		this._map = new mapboxgl.Map({
+			container: containerId,
+			style: 'mapbox://styles/mapbox/streets-v10',
+			center: [lat, lng],
+			zoom: 9
 		});
+	}
 
-		this._map.setView(new L.LatLng(lat, lng), 9);
-		this._map.addLayer(osm);
+	get firstStation() {
+		return this._firstStation;
+	}
+
+	set firstStation({geometry}) {
+		const {coordinates} = geometry;
+		const msg = "The first gas station";
+
+		this._firstStation = {
+			layers: {
+				marker: L.marker(coordinates, {title: msg})
+					.bindPopup(msg)
+					.openPopup(),
+				circle: L.circle(coordinates, {
+					color: 'blue',
+					fillColor: '#628cff',
+					fillOpacity: 0.1,
+					radius: 5000
+				})
+			}
+		};
+	}
+
+	get nearestStation() {
+		return this._nearestStation;
+	}
+
+	set nearestStation({geometry}) {
+		const {coordinates} = geometry;
+
+		this.nearestStation = {
+			layers: {
+				marker: L.marker(coordinates),
+				circle: L.circle(coordinates, {
+					color: 'yellow',
+					fillColor: '#f9ff09',
+					fillOpacity: 0.1,
+					radius: 5000
+				})
+			}
+		};
 	}
 
 	fetchFirstStation() {
-		fetch('http://localhost:3000')
+		return fetch('http://localhost:3000')
 			.then(response => response.json())
 			.then((json) => {
-				const {properties, geometry} = json;
-				this.setState({
-					name: properties.name,
-					coordinates: geometry.coordinates,
-				}, function () {
-					const {coordinates} = this.state;
-					const coords = [...coordinates].reverse();
-
-					const msg = "The first gas station";
-
-					this.firstStation = {
-						layers: {
-							marker: L.marker(coords, {title: msg})
-										.bindPopup(msg)
-										.openPopup(),
-							circle: L.circle(coords, {
-								color: 'blue',
-								fillColor: '#628cff',
-								fillOpacity: 0.1,
-								radius: 5000
-							})
-						}
-					};
-
-					if (this.map) {
-						Object.values(this.firstStation.layers)
-							.map(layer => layer.addTo(this.map));
-					}
-				});
+				this.firstStation = json;
 			});
 	}
 
 	fetchNearestStation({lat, lng}) {
-		fetch(`http://localhost:3000?lat=${lat}&lng=${lng}`, {method: "POST"})
+		return fetch(`http://localhost:3000?lat=${lat}&lng=${lng}`, {method: "POST"})
 			.then(response => response.json())
 			.then(json => {
-				const {properties, geometry} = json;
-				this.setState({
-					name: properties.name,
-					coordinates: geometry.coordinates,
-				}, function () {
-					const {coordinates} = this.state;
-					const coords = [...coordinates].reverse();
-
-					if (this.nearestStation && this.map) {
-						Object.values(this.nearestStation.layers)
-							.map(layer =>
-								this.map.hasLayer(layer) && this.map.removeLayer(layer)
-							);
-					}
-
-					this.nearestStation = {
-						layers: {
-							marker: L.marker(coords),
-							circle: L.circle(coords, {
-								color: 'yellow',
-								fillColor: '#f9ff09',
-								fillOpacity: 0.1,
-								radius: 5000
-							})
-						}
-					};
-
-					if (this.map) {
-						Object.values(this.nearestStation.layers)
-							.map(layer => layer.addTo(this.map));
-					}
-				});
+				this.nearestStation = json;
 			});
 	}
 
@@ -119,24 +98,68 @@ class MapComponent extends React.Component {
 	componentDidMount = () => {
 		this.map = {
 			containerId: 'mapid',
-			coords: {lat: 52.51, lng: 13.40}
+			coords: {lat: -74.50, lng: 40}
 		};
 
-		this.map.on('click', this.handleMapClick);
+		this.map.on('load', () => {
+			console.log('loaded');
+			this.map.addLayer({
+				id: 'rpd_parks',
+				type: 'fill',
+				source: {
+					type: 'vector',
+					url: 'mapbox://mapbox.3o7ubwm8'
+				},
+				'source-layer': 'RPD_Parks',
+				layout: {
+					visibility: 'visible'
+				},
+				paint: {
+					'fill-color': 'rgba(61,153,80,0.55)'
+				}
+			});
 
-		this.fetchFirstStation();
+			console.log(this.map);
+		});
+
+		// this.map.on('click', this.handleMapClick);
+
+		/*		this.fetchFirstStation()
+					.then(() => {
+						if (this.map) {
+							Object.values(this.firstStation.layers)
+								.map(layer => layer.addTo(this.map));
+						}
+					});*/
 	}
 
 	handleMapClick = event => {
-		this.fetchNearestStation(event.latlng);
+		/*		this.fetchNearestStation(event.latlng)
+					.then(() => {
+						if (this.nearestStation && this.map) {
+							Object.values(this.nearestStation.layers)
+								.map(layer =>
+									this.map.hasLayer(layer) && this.map.removeLayer(layer)
+								);
+						}
+
+						if (this.map) {
+							Object.values(this.nearestStation.layers)
+								.map(layer => layer.addTo(this.map));
+						}
+
+						const {lat: flat, lng: flng} = this.firstStation.layers.marker.getLatLng();
+
+						const {lat: nlat, lng: nlng} = this.nearestStation.layers.marker.getLatLng();
+					});*/
 	}
 
 	render() {
-		const {name, coordinates} = this.state;
+		// const {name, coordinates} = this.state;
 		return (
 			<div>
-				Name: {name}<br/>
-				{coordinates[1]} {coordinates[0]}
+				{/*Name: {name}<br/>*/}
+				{/*{coordinates[1]} {coordinates[0]}*/}
 				<div id="mapid"></div>
 			</div>);
 	}
